8ce13be5ec91f53bf6dbb32da5dd9d95b9f02a4c
Tyler Bailey <tyler.bailey@10up.com>
2024-06-11 12:22:53 -0400

    Add the synced property to the patterns API schema and ensure its added to the response.

--- lib/compat/wordpress-6.7/class-gutenberg-rest-block-patterns-controller.php
+++ lib/compat/wordpress-6.7/class-gutenberg-rest-block-patterns-controller.php
@@ -9,46 +9,46 @@
 /**
  * Core class used to access block patterns via the REST API.
  *
- * @see Gutenberg_REST_Block_Patterns_Controller
+ * @see Gutenberg_REST_Block_Patterns_Controller_6_7
  */
 class Gutenberg_REST_Block_Patterns_Controller_6_7 extends Gutenberg_REST_Block_Patterns_Controller {
 	/**
-	 * Prepare a raw block pattern before it gets output in a REST API response.
+	 * Add the `synced` property to the block pattern response.
 	 *
 	 * @since 6.0.0
-	 * @since 6.3.0 Added `source` property.
+	 * @since 6.7.0 Added `synced` property.
 	 *
 	 * @param array           $item    Raw pattern as registered, before any changes.
 	 * @param WP_REST_Request $request Request object.
 	 * @return WP_REST_Response|WP_Error Response object on success, or WP_Error object on failure.
 	 */
 	public function prepare_item_for_response( $item, $request ) {
-		$fields = $this->get_fields_for_response( $request );
-		$keys   = array(
-			'name'          => 'name',
-			'title'         => 'title',
-			'content'       => 'content',
-			'description'   => 'description',
-			'viewportWidth' => 'viewport_width',
-			'inserter'      => 'inserter',
-			'categories'    => 'categories',
-			'keywords'      => 'keywords',
-			'blockTypes'    => 'block_types',
-			'postTypes'     => 'post_types',
-			'templateTypes' => 'template_types',
-			'source'        => 'source',
-			'synced'        => 'synced',
-		);
-		$data   = array();
-		foreach ( $keys as $item_key => $rest_key ) {
-			if ( isset( $item[ $item_key ] ) && rest_is_field_included( $rest_key, $fields ) ) {
-				$data[ $rest_key ] = $item[ $item_key ];
-			}
+		$response = parent::prepare_item_for_response( $item, $request );
+
+		$response->data['synced'] = $item['synced'] ?? false;
+		return rest_ensure_response( $response );
+	}
+
+	/**
+	 * Add the `synced` property to the block pattern schema.
+	 *
+	 * @since 6.0.0
+	 * @since 6.7.0 Added `source` property.
+	 *
+	 * @return array Item schema data.
+	 */
+	public function get_item_schema() {
+		$schema = parent::get_item_schema();
+		if ( ! isset( $schema['properties']['synced'] ) ) {
+			$schema['properties']['synced'] = array(
+				'description' => __( 'Whether the pattern is synced or static.', 'gutenberg' ),
+				'type'        => 'boolean',
+				'readonly'    => true,
+				'context'     => array( 'view', 'edit', 'embed' ),
+			);
 		}
 
-		$context = ! empty( $request['context'] ) ? $request['context'] : 'view';
-		$data    = $this->add_additional_fields_to_object( $data, $request );
-		$data    = $this->filter_response_by_context( $data, $context );
-		return rest_ensure_response( $data );
+		$this->schema = $schema;
+		return $this->add_additional_fields_schema( $this->schema );
 	}
 }
--- lib/compat/wordpress-6.7/rest-api.php
+++ lib/compat/wordpress-6.7/rest-api.php
@@ -12,6 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /**
  * Registers the block patterns REST API routes.
+ * Use a -1 priority to ensure that this action runs first.
  */
 function gutenberg_register_rest_block_patterns_routes_6_7() {
 	$block_patterns = new Gutenberg_REST_Block_Patterns_Controller_6_7();
